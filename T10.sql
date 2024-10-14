8c6878153e3bfc05b6761969c6a83ae452a251c6
pranitavaidya <pranuarekar156@gmail,com>
2024-10-14 09:00:43 +0530

    fourth commit

---
+++ T10.sql
@@ -0,0 +1,6 @@
+-- alter table employee_database add column country varchar;
+
+ -- update employee_database set country = case
+ -- when IsActive = True then 'India'
+ -- when IsActive = False then 'U.S.A'
+ -- END;
---
+++ T2.sql
@@ -0,0 +1,7 @@
+insert into employee_details(FirstName, LastName, Email, PhoneNumber,
+HireDate, Salary, DepartmentID, IsActive, JobTitle) 
+values('Ashwini','Sarode','sarodeashwini@gmail.com',9011064225,'2024-3-10',25000.00,3,True,'Data analytics'),
+('Pranita','Vaidya','vaidyapranita@gmail.com',9611064225,'2024-4-11',45000.00,4,False,'Marketing analytics'),
+('Harshad','Sharma','sharmaharshad@gmail.com',9021074225,'2024-5-9',28000.00,5,True,'Finance analytics'),
+('Vivek','Harne','harnevivek@gmail.com',9811064225,'2024-3-7',35000.00,6,False,'software analytics'),
+('Chetak','Varma','varmachetak@gmail.com',9711064225,'2024-8-8',55000.00,7,True,'Java analytics');
